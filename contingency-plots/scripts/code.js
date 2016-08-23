9165386aa489f292980605dff3d48a7d41be82e3
Steve <stweinn@yahoo.com>
2016-08-23 14:51:40 +0000

    lots of improvements: caching settings, improving maps, research options, stats that update as you move

--- contingency-plots/scripts/code.js
+++ contingency-plots/scripts/code.js
@@ -1,43 +1,86 @@
 var baseurl = "http://api.openhouseproject.co"
 
-var response = null
+var save_var_name = "oh-state"
 
-function render() {
-  var min_price = 0
-  var max_price = 1000000000
-  svals = sliders['bed'].noUiSlider.get()
-  var min_beds = parseFloat(svals[0])
-  var max_beds = parseFloat(svals[1])
-  svals = sliders['bath'].noUiSlider.get()
-  var min_baths = parseFloat(svals[0])
-  var max_baths = parseFloat(svals[1])
-  svals = sliders['sqft'].noUiSlider.get()
-  var min_sqft = parseFloat(svals[0])
-  var max_sqft = parseFloat(svals[1])
+function isRadius(s) {
+  if (s.split(",").length!=3) return false;
+  if (s.substring(0,1)!="(") return false;
+  if (s.substring(s.length-1, s.length)!=")") return false;
+  return true;
+}
+
+function default_stats() {
+  var zeros = {'min': 0, 'max': 0, 'mean': 0, 'stdev': 0, 'median': 0}
+  return {'beds': zeros, 'baths': zeros, 'price': zeros, 'sqft': zeros}
+}
+
+function get_stats(arr) {
+  var stats = {
+      'min': math.min(arr)
+    , 'max': math.max(arr)
+    , 'mean': math.mean(arr)
+    , 'median': math.median(arr)
+    , 'std': math.std(arr)
+  }
+  return stats
+}
+
+function get_request() {
   svals = sliders['price'].noUiSlider.get()
+  state['min_price'] = parseFloat(svals[0])
+  state['max_price'] = parseFloat(svals[1])
+  svals = sliders['bed'].noUiSlider.get()
+  state['min_beds'] = parseFloat(svals[0])
+  state['max_beds'] = parseFloat(svals[1])
+  svals = sliders['bath'].noUiSlider.get()
+  state['min_baths'] = parseFloat(svals[0])
+  state['max_baths'] = parseFloat(svals[1])
+  svals = sliders['sqft'].noUiSlider.get()
+  state['min_sqft'] = parseFloat(svals[0])
+  state['max_sqft'] = parseFloat(svals[1])
+  svals = sliders['price'].noUiSlider.get()
+  address = $("#address").val()
+  address = address.trim()
 
-	request = {'min_price': min_price,
-      'max_price': max_price,
-      'min_bedrooms': min_beds,
-      'max_bedrooms': max_beds,
-      'min_bathrooms': min_baths,
-      'max_bathrooms': max_baths,
-      'min_building_size': min_sqft,
-      'max_building_size': max_sqft,
+  request = {'min_price': state['min_price'],
+      'max_price': state['max_price'],
+      'min_bedrooms': state['min_beds'],
+      'max_bedrooms': state['max_beds'],
+      'min_bathrooms': state['min_baths'],
+      'max_bathrooms': state['max_baths'],
+      'min_building_size': state['min_sqft'],
+      'max_building_size': state['max_sqft'],
       'limit': '500',
       'offset': '0'
   }
+  if (isRadius(address)) {
+    request['close_to'] = address
+  } else {
+    request['address'] = address
+  }
+  return request
+}
 
+function update_curl_req(request) {
   var murl = baseurl + '/api/property/?'
   var keys = Object.keys(request)
   for (var i in keys) {
-  	var item = keys[i]
-  	if (i > 0) {
-  		murl += "&"
-  	}
-  	murl += item + "=" + request[item]
+    var item = keys[i]
+    if (i > 0) {
+      murl += "&"
+    }
+    murl += item + "=" + request[item]
+    console.log("saving state")
+    localStorage.setItem(save_var_name, JSON.stringify(state))
   }
   $("#curl").val('curl -X GET "' + murl + '"')
+  return murl
+}
+
+function render() {
+  request = get_request()
+  murl = update_curl_req(request)
+
   $.ajax({
   	url: murl,
   	type: 'GET',
@@ -47,7 +90,7 @@ function render() {
       response = resp
       $(".wait-spinner").hide()
       updateTable(resp)
-      updateMap(resp)
+      updateMap(resp['results'])
       makePlots(resp)
   	},
   	error: function() {
@@ -85,32 +128,100 @@ function makePlots(resp) {
   }
 }
 
-function updateMap(resp) {
-    if (resp['count'] > 0) {
-      data = resp['results']
+function updateMap(data) {
+    remove_all_markers()
+    stats = default_stats()
+
+    if (data.length > 0) {
+      beds = []
+      baths = []
+      sqfts = []
+      prices = []
       var latlngs = []
       var error_count = 0
       $.each(data, function(i, elem) {
         var ao = elem['address_object']
+        var bed = elem['bedrooms']
+        var bath = elem['bathrooms']
+        var price = elem['price']
+        var sqft = elem['building_size']
         if (ao != null) {
           property = {
             "address": ao['formatted_address'],
-            "sale_price": elem['price'],
-            "bedrooms": elem['bedrooms'],
-            "bathrooms": elem['bathrooms'],
+            "sale_price": price,
+            "bedrooms": bed,
+            "bathrooms": bath,
             "latitude": ao['latitude'],
             "longitude": ao['longitude']
           }
           add_marker(property)
+          if (bed >= 0) {
+            beds.push(bed)
+          }
+          if (bath >= 0) {
+            baths.push(bath)
+          }
+          if (price >= 0) {
+            prices.push(price)
+          }
+          if (sqft >= 0) {
+            sqfts.push(sqft)
+          }
         } else {
           error_count += 1
         }
       })
+      if (beds.length > 0) {
+        stats['beds'] = get_stats(beds)
+      }
+      if (baths.length > 0) {
+        stats['baths'] = get_stats(baths)
+      }
+      if (prices.length > 0) {
+        stats['price'] = get_stats(prices)
+      }
+      if (sqfts.length > 0) {
+        stats['sqft'] = get_stats(sqfts)
+      }
       console.log("There were " + error_count + " listings missing `address_object`")
     }
     else {
       console.log("No listings to update")
     }
+    update_summary('summary')
+}
+
+function formatNumber(n) {
+  return Math.round(n * 100)/100
+}
+
+function update_summary(container) {
+  var bdy = `
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Mean</th>
+          <th>Median</th>
+        </tr>
+      </thead>
+      <tbody>
+      `
+  var arr = ['price', 'beds', 'baths', 'sqft']
+  $.each(arr, function(i, m) {
+    bdy += `
+        <tr>
+          <td>` + m + `</td>
+          <td>` + formatNumber(stats[m]['mean']) + `</td>
+          <td>` + formatNumber(stats[m]['median']) + `</td>
+        </tr>
+    `
+  })
+  bdy += `
+      </tbody>
+    </table>
+  `
+  $("#" + container).html(bdy)
 }
 
 function extractData(data, var1, var2) {
@@ -159,34 +270,132 @@ function aggregateData(data, var1, var2) {
 
 function showWaiting() {
   $("#errorBox").hide()
+  $("#searchAgain").hide()
   $(".wait-spinner").show()
 	$(".plotCell").html("<img src='box.gif' />")
 }
 
-onchange = function() {
+function load_state() {
+  var pstate = localStorage.getItem(save_var_name);
+  if (pstate == null) {
+    console.log("Fresh state")
+    state['min_price'] = 100000
+    state['max_price'] = 10000000
+    state['min_sqft']  = 0
+    state['max_sqft']  = 10000
+    state['address']   = ""
+    localStorage.setItem(save_var_name, JSON.stringify(state))
+  }
+  else {
+    console.log("Loading state")
+    state = JSON.parse(pstate)
+  }
+}
+
+
+var map = null
+var sliders = {}
+var state = {}
+
+function between(val, bound1, bound2) {
+  if (bound2 < bound1) {
+    swap = bound1
+    bound1 = bound2
+    bound2 = swap
+  }
+  if (val >= bound1) {
+    if (val <= bound2) {
+      return true
+    }
+  }
+  return false
+}
+
+function considerMapUpdate() {
+  $("#searchAgain").show()
+  var inbounds = []
+  if (window['response'] != undefined) {
+    var bounds = map.getExtent().clone()
+    bbox = bounds.transform(map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"))
+    $.each(response['results'], function(i, result) {
+      ao = result['address_object']
+      if (ao != undefined) {
+        if (between(ao['latitude'], bbox.bottom, bbox.top)) {
+          if (between(ao['longitude'], bbox.left, bbox.right)) {
+            inbounds.push(result)
+          }
+        }
+      }
+    })
+    updateMap(inbounds)
+  }
+}
+
+function mapEvent(event) {
+  et = event.type
+  if (et=="zoomend" || et=="moveend") {
+    considerMapUpdate()
+  } else {
+    console.log(event.type)    
+  }
+}
+function mapBaseLayerChanged(event) {
+  console.log(event.type + " " + event.layer.name);
+}
+function mapLayerChanged(event) {
+  console.log(event.type + " " + event.layer.name + " " + event.property);
+}
+
+function doSearch() {
   showWaiting()
   render()
 }
 
-var map = null
-var sliders = {}
-
 $( document ).ready(function() {
-  max_price=10000000
-  min_price=  100000
-  max_sqft=10000
+  load_state()
 
-  $("#btnSearch").click(function() {
-    showWaiting()
-    render()
+  var response = null
+  var stats = default_stats()
+
+  $("#btnSearch").click(doSearch)
+  $("#btnSearchYes").click(function() {
+    var bounds = map.getExtent().clone()
+    bbox = bounds.transform(map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"))
+    clon = (bbox.top - bbox.bottom)/2 + bbox.bottom
+    clat = (bbox.left - bbox.right)/2 + bbox.right
+
+    var lat2 = bbox.left
+    var lon2 = bbox.top
+
+    miles = haversine_distance(clat, clon, lat2, lon2)
+
+    searchBounds = "(" + Math.round(miles) + "," + clon.toFixed(3) + "," + clat.toFixed(3) + ")"
+    $("#address").val(searchBounds)
+    doSearch()
+  })
+  $("#btnSearchNo").click(function() {
+    $("#searchAgain").hide()
   })
 
   sliders['bed'] = slider("#slide_bed", -1, 6, [1, 3], "Beds: ")
   sliders['bath'] = slider("#slide_bath", -1, 4, [1, 2], "Baths: ")
-  sliders['price'] = slider("#slide_price", -1, max_price, [min_price, max_price], "Price: ")
-  sliders['sqft'] = slider("#slide_sqft", -1, max_sqft, [-1, max_sqft], "SQ.FT.: ")
+  sliders['price'] = slider("#slide_price", -1, state['max_price'], [state['min_price'], state['max_price']], "Price: ")
+  sliders['sqft'] = slider("#slide_sqft", -1, state['max_sqft'], [state['min_sqft'], state['max_sqft']], "SQ.FT.: ")
 
-  mapInit('map', 'info', [], 39.50, -98.35, 4)
+  $("#address").val(state['address'])
+
+  ev = {
+      eventListeners: {
+          "moveend": mapEvent,
+          "zoomend": mapEvent,
+          "changelayer": mapLayerChanged,
+          "changebaselayer": mapBaseLayerChanged
+      }
+    }
+  
+  map = mapInit('map', 'info', [], 39.50, -98.35, 4, ev)
+
+  $("#searchAgain").hide()
 
   new Clipboard('#btnCopy');
 
@@ -197,6 +406,14 @@ $( document ).ready(function() {
           var active = $('#tabs').tabs('option', 'active');
       }
   });
+  $.each(sliders, function(i, slider) {
+    slider.noUiSlider.on('update', function( values, handle ) {
+      update_curl_req(get_request())
+    })
+  })
+  $("#address").change(function() {
+    update_curl_req(get_request())
+  })
 });
 
 
