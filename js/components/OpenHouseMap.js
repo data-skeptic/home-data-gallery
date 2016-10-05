4ae01586a5941105403fd8ec26c879b73a0a8a65
stweinn <stweinn@yahoo.com>
2016-10-05 02:11:22 +0100

    work in progress on map

--- js/components/OpenHouseMap.js
+++ js/components/OpenHouseMap.js
@@ -1,81 +1,35 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {Map, Marker, Popup, TileLayer} from "react-leaflet";
-
-//var Map = require('react-d3-map').Map;
-//var MarkerGroup = require('react-d3-map').MarkerGroup;
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import Markers from './Markers'
 
 export default class OpenHouseMap extends React.Component {
   
   constructor(props) {
-  	super(props)
-  	this.state = {
-  		lat: 51.505,
-  		lng: -0.09,
-  		zoom: 13
-  	}
+    super(props);
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13,
+    };
   }
 
   render() {
     const position = [this.state.lat, this.state.lng];
-    return (<Map id="map-container" center={position} zoom={this.state.zoom}>
-      <TileLayer
-        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-        </Popup>
-      </Marker>
-    </Map>)
-    /*
-    const center = [this.state.lat, this.state.lng];
-    var width = 700;
-    var height = 700;
-    var scale = 1200 * 5;
-    var scaleExtent = [1 << 12, 1 << 13]
-
-    var popupContent = function(d) { console.log(1); return d.properties.text; }
-
-    var data = {"type": "FeatureCollection","features": []}
-    var listings = this.props.listings
-    for (var i=0; i < listings.length; i++) {
-      var listing = listings[i]
-      if (listing['address_object'] !== undefined) {
-        var lat = listing['address_object']['latitude']
-        var lon = listing['address_object']['longitude']
-        var nlisting = {
-                    "type": "Feature",
-                    "properties": {
-                      "text": listing['raw_address']
-                    },
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [lat, lon]
-                    }
-                  }
-        data['features'].push(nlisting)
-      }
-    }
+    return (<div>Map here</div>)
     return (
-        <Map
-          width= {width}
-          height= {height}
-          scale= {scale}
-          scaleExtent= {scaleExtent}
-          center= {center}
-        >
-          <MarkerGroup
-            key= {"polygon-test"}
-            data= {data}
-            popupContent= {popupContent}
-            markerClass= {"your-marker-css-class"}
-          />
-        </Map>
+      <Map class='ohmap' center={position} zoom={this.state.zoom}>
+       <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
     );
-    */
   }
 }
