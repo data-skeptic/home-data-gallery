ac082aab3b515077adc0e5c1eb6df72745299218
Kyle Polich <kyle@datascience.com>
2016-10-14 14:09:20 -0700

    integrating react plots

---
+++ js/components/BarComp.js
@@ -0,0 +1,24 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import rd3 from 'react-d3'
+
+export default class BarComp extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+	var BarChart = rd3.BarChart;
+	return (<div>
+		      <BarChart
+			      data={this.props.data}
+			      width={this.props.width}
+			      height={this.props.height}
+			      title={this.props.title}
+			      xAxisLabel={this.props.xAxisLabel}
+			      yAxisLabel={this.props.yAxisLabel}
+			      />
+	       </div>)
+	}
+}
---
+++ js/components/ScatterComp.js
@@ -0,0 +1,25 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import rd3 from 'react-d3'
+
+export default class ScatterComp extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+	var ScatterChart = rd3.ScatterChart;
+	return (<div>
+				<ScatterChart
+				data={this.props.data}
+				width={this.props.width}
+				height={this.props.height}
+				title={this.props.title}
+				xAxisLabel={this.props.xAxisLabel}
+				yAxisLabel={this.props.yAxisLabel}
+				domain={this.props.domain}
+				/>
+	       </div>)
+	}
+}
