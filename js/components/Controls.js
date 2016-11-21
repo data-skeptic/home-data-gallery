402c672cb537ed0acc67252f759966202d54ec46
Kyle Polich <kylepolich@gmail.com>
2016-11-21 08:49:56 -0800

    Feature/44 rfiddle integration (#57)
    * Add button to link to R-Fiddle
    
    * Correct were to we're on loading messages
    
    * Add in class to store the sample r code. Not quite sure this is the best way to go
    
    * Concatenate the value to the variable actually gets evaluated instead of the string just coming through
    
    * Add in the sample R code to be passed over to the rFiddle service.

--- js/components/Controls.js
+++ js/components/Controls.js
@@ -34,13 +34,13 @@ export default class Controls extends React.Component {
     if (this.props.busy || this.props.changed) {
       if (this.props.offset == 0 && this.props.count == 1) {
         loadingMessage = <div class="alert alert-info" role="alert">
-                            <strong>Hang fire a second!</strong> Were looking for properties in this area.
+                            <strong>Hang fire a second!</strong> We&apos;re looking for properties in this area.
                             <img src="box.gif" class="float-xs-right" alt="Loading image" width="40"/>
                           </div>
       }
       else {
         loadingMessage = <div class="alert alert-info" role="alert">
-                            <strong>Hang fire a second!</strong> Were checking for more listings...
+                            <strong>Hang fire a second!</strong> We&apos;re checking for more listings...
                             <img src="box.gif" class="float-xs-right" alt="Loading image" width="40"/>
                           </div>
       }
@@ -74,23 +74,45 @@ export default class Controls extends React.Component {
           <p>Area (Sq.Ft)</p>
           <Slider title='sq.ft.' min_value={0} max_value={10000} low={this.props.searchCriteria.sqft[0]} high={this.props.searchCriteria.sqft[1]} onUpdate={this.onUpdate.bind(this, 'sqft')} />
         </div>
-      </div>
-      
-      <div class="row">
-        <form class="form">
-          <div class="form-group">
-            <label for="cURL_textbox">cURL Request: </label>
-            <div class="input-group">
-              <input type="text" class="form-control" id="cURL_textbox" value={this.state.value} onChange={({target: {value}}) => this.setState({value, copied: false})}/>
-              <div class="input-group-addon">
-              <CopyToClipboard text={this.state.value} onCopy={() => this.setState({copied: true})}>
-              <buton>Copy <img src="copy.png" id='copyPng' alt="Copy to clipboard" /></buton>
-                </CopyToClipboard>
+
+        <div class="col-sm-4">
+          <form class="form">
+            <div class="form-group">
+              <label for="cURL_textbox">cURL Request: </label>
+              <div class="input-group">
+                <input type="text" class="form-control" id="cURL_textbox" value={this.state.value} onChange={({target: {value}}) => this.setState({value, copied: false})}/>
+                <div class="input-group-addon">
+                <CopyToClipboard text={this.state.value} onCopy={() => this.setState({copied: true})}>
+                <buton>Copy <img src="copy.png" id='copyPng' alt="Copy to clipboard" /></buton>
+                  </CopyToClipboard>
+                </div>
               </div>
             </div>
-          </div>
-          {this.state.copied ? <span style={{color: 'red'}}> Copied.</span> : null}
-        </form>
+            {this.state.copied ? <span style={{color: 'red'}}> Copied.</span> : null}
+          </form>
+          <pre class="rfiddleHelper" hidden>
+{`# Pull in the rjson library to parse the JSON output from the OpenHouseProject API
+library("rjson")
+
+# Fetch the json data the API
+json_data <- fromJSON(file="` + 
+this.state.value + 
+`")
+
+#Mold this into a data frame
+properties_frame <- lapply(json_data$results, function(x) {
+  x[sapply(x, is.null)] <- NA
+  unlist(x)
+})
+properties <- do.call("rbind", properties_frame)
+
+# Grab the prices as numeric type and plot a basic histogram on the data
+prices <- as.numeric(as.character(properties[,19]))
+hist(prices)
+`}
+          </pre>
+          <a class="btn btn-primary" href={"http://www.r-fiddle.org/#/query/fiddle?code=" + encodeURIComponent($('pre.rfiddleHelper').text())} target="_blank">Open dataset in R-Fiddle</a>
+        </div>
       </div>
 
       <div class="row">
