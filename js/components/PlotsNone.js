eb8b9ee8b40dc90d732dfb24e346546df9f9acd0
Billy Everyteen <kyle@datascience.com>
2017-01-09 14:11:13 -0800

    fix submission form

--- js/components/PlotsNone.js
+++ js/components/PlotsNone.js
@@ -12,19 +12,27 @@ export default class PlotsNone extends Component {
 	constructor(props) {
     	super(props)
     	this.state = {
-    		result: "none"
+    		result: "none",
+    		email: "",
+    		url: "http://",
+    		cb_notify: true
     	}
     	this.onSubmit = this.onSubmit.bind(this)
     }
+    onUpdate(e) {
+    	console.log(e.target)
+    	var att = e.target.id
+    	var value = e.target.value
+    	var u = {}
+    	u[att] = value
+    	console.log(u)
+    	this.setState(u)
+    }
     onSubmit() {
 	    var api = "https://5xwvsgjnqi.execute-api.us-east-1.amazonaws.com/prod/OH-submit-url"
-	    var email = $("#email").val()
-	    var url = $("#url").val()
-	    var c = $("#cb_notify").attr('checked')
-	    var checked = true
-	    if (c == undefined) {
-	      checked = false
-	    }
+	    var email = this.state.email
+	    var url = this.state.url
+	    var checked = this.state.cb_notify
 	    var res = {"email": email, "url": url, "checked": checked}
 	    this.setState({result: "submitting"})
 	    var me = this
@@ -66,19 +74,19 @@ export default class PlotsNone extends Component {
 				    <tbody>
 					    <tr>
 					      <td>URL:</td>
-					      <td><input id="url" class="box" type="text" value="http://" /></td>
+					      <td><input id="url" class="box" type="text" value={this.state.url} onChange={this.onUpdate.bind(this)} /></td>
 					    </tr>
 					    <tr>
 					      <td>Email:</td>
-					      <td><input id="email" class="box" type="text" value="" /></td>
+					      <td><input id="email" class="box" type="text" value={this.state.email} onChange={this.onUpdate.bind(this)} /></td>
 					    </tr>
 					    <tr>
 					      <td></td>
-					      <td><input type="checkbox" id="cb_notify" checked />Notify me about updates related to this data</td>
+					      <td><input type="checkbox" id="cb_notify" checked={this.state.cb_notify} onChange={this.onUpdate.bind(this)} />Notify me about updates related to this data</td>
 					    </tr>
 					    <tr>
 					      <td></td>
-					      <td align="right">
+					      <td>
 					      	<button id="btnSubmit" onClick={this.onSubmit.bind(this)}>Submit</button>
 					      </td>
 					    </tr>
